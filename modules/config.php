9bfd88f67fc143dc59f205424f104044468a52d3
Atha3417 <atha.3417@gmail.com>
2021-12-26 18:27:33 +0700

    upload file

--- modules/config.php
+++ modules/config.php
@@ -1,27 +1,11 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/functions.php';
-
-function env($key, $default = null)
-{
-    $value = getenv($key);
-    if ($value === false) {
-        return $default;
-    }
-    return $value;
-}
 
 $configs = [
     'db' => [
-        'driver' => env('DB_DRIVER', 'mysql'),
-        'host' => env('DB_HOST', 'localhost'),
-        'name' => env('DB_NAME', 'storage'),
-        'user' => env('DB_USER', 'root'),
-        'pass' => env('DB_PASS', ''),
-    ],
-    'app' => [
-        'name' => env('APP_NAME', 'Storage'),
-        'url' => env('APP_URL', 'https://storage.me'),
-        'timezone' => env('APP_TIMEZONE', 'UTC'),
+        'driver' => 'mysql',
+        'host' => '45.143.81.1',
+        'name' => 'u1548240_storage',
+        'user' => 'u1548240_athatsaqif',
+        'pass' => 'htmlcssjsphp'
     ]
 ];
